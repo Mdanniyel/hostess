0665ed383094016aa552e3c439217aa1d4194463
Mdanniyel <41233756+Mdanniyel@users.noreply.github.com>
2026-06-06 23:00:44 +0300

    chore: update spa fallback to 200.html and synchronize 404.html during build

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ fallback: '404.html' }),
+		adapter: adapter({ fallback: '200.html' }),
 		paths: {
 			base: process.env.BASE_PATH || '/he-IL/corp/hostess-app',
 			relative: false
